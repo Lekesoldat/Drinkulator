bf16301c50616a985adddaa4304238bd994c2d9e
Lekesoldat <magnulho@gmail.com>
2019-03-13 15:59:21 +0100

    Implement onSearchSubmit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import bolaget from '../api/bolaget';
+import SearchBar from './SearchBar';
+
+const search = params => bolaget.get("/products",  { params } );
 
 class App extends React.Component {
   state = {
-    results: []
+    results: {}
   };
+
+  onSearchSubmit = async term => {
+    const response = await search("/products", {search: term});
+    this.setState({results: response.data})
+  }
 
   render() {
     return (
-      <div>Drinkulator is going down</div>
+      <div className="ui container" style={{marginTop: '10px'}}>
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+      </div>
     )}
 }
 
