b6dfc3670da8b9cbdfefcb60669ff87f72b4c33b
Lekesoldat <magnulho@gmail.com>
2019-03-13 15:59:10 +0100

    Implement onFormSubmit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,12 +2,27 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   state = {term: ''};
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  }
   
   render() {
     return(
-      <div>SearchBar</div>
-  )};
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+          <label>Drink Search</label>
+          <input
+            type="text"
+            value={this.state.term}
+            onChange={e => this.setState({term: e.target.value})}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
-
-
 export default SearchBar;
