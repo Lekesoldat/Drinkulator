02dde0746e0de4ab80ef97bc6423cc7d25124ba9
Lekesoldat <magnulho@gmail.com>
2019-03-13 17:12:23 +0100

    Add styling and values to be returned.

--- src/components/DrinkList.js
+++ src/components/DrinkList.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const DrinkList = props => {
   const results = props.results.map(result => {
-    return <div>{result.nr}</div>;
+    return (
+      <div className="ui segment">
+        <div>Name: {result.name}</div>
+        <div>Additional name: {result.additional_name}</div>
+        <div>Price: {result.price.amount} {result.price.currency}</div>
+      </div>
+    )
   });
 
   return <div>{results}</div>;
