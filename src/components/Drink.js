1df5ede4890faa4ce5fe6e8b38021b3cd93821a9
Lekesoldat <magnulho@gmail.com>
2019-03-15 15:59:10 +0100

    Implement Card.

--- src/components/Drink.js
+++ src/components/Drink.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React from 'react';
+import { Card } from 'semantic-ui-react';
+import { MLperNOK, NOKperML } from '../calculation';
 
+// TODO: Fix additional_name can be null
 class Drink extends React.Component {
   render() {
-    return <div>Drink</div>;
+    return (
+      <Card
+        header={
+          this.props.result.name + ' ' + this.props.result.additional_name
+        }
+        meta={
+          NOKperML(
+            this.props.result.volume_in_milliliter,
+            parseFloat(this.props.result.alcohol),
+            this.props.result.price.amount
+          ) + ' kr/ml alkohol'
+        }
+        description={'Volume: ' + this.props.result.volume_in_milliliter + 'ml'}
+      />
+    );
   }
 }
 
--- src/components/DrinkList.js
+++ src/components/DrinkList.js
@@ -1,30 +1,14 @@
 import React from 'react';
 import { MLperNOK, NOKperML } from '../calculation';
+import Drink from './Drink';
+import { Card } from 'semantic-ui-react';
 
 const DrinkList = props => {
   const results = props.results.map(result => {
-    return (
-      <div className='ui segment'>
-        <div>Name: {result.name}</div>
-        <div>Additional name: {result.additional_name}</div>
-        <div>Volume: {result.volume_in_milliliter}</div>
-        <div>Alcohol: {result.alcohol}</div>
-        <div>
-          Price: {result.price.amount} {result.price.currency}
-        </div>
-        <div>
-          SEK / Millilitre alcohol{' '}
-          {NOKperML(
-            result.volume_in_milliliter,
-            parseFloat(result.alcohol),
-            result.price.amount
-          )}{' '}
-        </div>
-      </div>
-    );
+    return <Drink result={result} />;
   });
 
-  return <div>{results}</div>;
+  return <Card.Group>{results}</Card.Group>;
 };
 
 export default DrinkList;
