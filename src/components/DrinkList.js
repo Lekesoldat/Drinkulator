6b22c56e8929e039bc2e32fded786af9f9445791
Lekesoldat <magnulho@gmail.com>
2019-03-15 17:58:02 +0100

    Fix empty result render bug.

--- src/components/DrinkList.js
+++ src/components/DrinkList.js
@@ -3,11 +3,15 @@ import Drink from './Drink';
 import { Card } from 'semantic-ui-react';
 
 const DrinkList = props => {
+  if (props.results.length === 0) {
+    return null;
+  }
+
   const results = props.results.map(result => {
     return <Drink result={result} />;
   });
 
-  return <Card.Group>{results}</Card.Group>;
+  return <Card.Group centered>{results}</Card.Group>;
 };
 
 export default DrinkList;
