1a8b0a0a409d14e9d3fb751697460017bcf4d9de
Lekesoldat <magnulho@gmail.com>
2019-03-15 16:07:30 +0100

    Add spacing and TODO

--- src/calculation.js
+++ src/calculation.js
@@ -1,7 +1,10 @@
 export const MLperNOK = (volume, percentage, price) =>
   ((volume * (percentage / 100)) / price).toFixed(2);
+
 export const NOKperML = (volume, percentage, price) =>
   (price / ((volume * percentage) / 100)).toFixed(2);
+
+// TODO: Implement asTubis
 
 export const tubis = {
   name: 'Tuborg Grønn',
