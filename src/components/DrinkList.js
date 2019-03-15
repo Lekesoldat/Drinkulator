0e913752d1874f69f71fdce6e018f3bab92a7339
Lekesoldat <magnulho@gmail.com>
2019-03-15 15:08:45 +0100

    Display NOKperML.

--- src/components/DrinkList.js
+++ src/components/DrinkList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MLperNOK, NOKperML } from '../calculation';
 
 const DrinkList = props => {
   const results = props.results.map(result => {
@@ -6,8 +7,18 @@ const DrinkList = props => {
       <div className='ui segment'>
         <div>Name: {result.name}</div>
         <div>Additional name: {result.additional_name}</div>
+        <div>Volume: {result.volume_in_milliliter}</div>
+        <div>Alcohol: {result.alcohol}</div>
         <div>
           Price: {result.price.amount} {result.price.currency}
+        </div>
+        <div>
+          SEK / Millilitre alcohol{' '}
+          {NOKperML(
+            result.volume_in_milliliter,
+            parseFloat(result.alcohol),
+            result.price.amount
+          )}{' '}
         </div>
       </div>
     );
